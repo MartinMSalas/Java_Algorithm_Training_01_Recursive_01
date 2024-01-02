d7efcf3290dda000b4ce5a9dc4bf1768605ffb73
MartinMSalas <ingmartinmsalas@gmail.com>
2024-01-02 00:34:24 -0300

    feat: add fibonacci funct

--- RecursiveExample01/src/Main.java
+++ RecursiveExample01/src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -11,12 +15,31 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
-        System.out.println(factorial(-5));
+        //System.out.println(factorial(-5));
+
+        System.out.println(fibonacci(40));
+        System.out.println(fibonacci2(40));
+
+
     }
 
     public static int factorial(int n){
         System.out.println("n = " + n);
         if (n < 2) return 1;
         return n*factorial(n-1);
+    }
+    public static ArrayList<Integer> fibonacci(int n){
+        ArrayList<Integer> fibo = new ArrayList<>(Arrays.asList(0, 1));
+        if(n<3) return fibo;
+        return fillFibonacci(fibo, n);
+    }
+    private static ArrayList<Integer> fillFibonacci(ArrayList<Integer> fibo, int n){
+        if(fibo.size() == n) return fibo;
+        fibo.add(fibo.get(fibo.size()-1) + fibo.get(fibo.size()-2));
+        return fillFibonacci(fibo, n);
+    }
+    public static long fibonacci2(long n){
+        if(n<2) return n;
+        return fibonacci2(n-1) + fibonacci2(n-2);
     }
 }
